21ad5397c83d5664f42ce2b0159719f686631eb4
Franklin Oliveira <franklin.alves.oliveira@gmail.com>
2019-07-28 21:21:38 -0300

    Primeira versão totalmente operacional. Ponto a ser resolvido: otimizar o site para carregar mais rápido

--- hw1/routes.py
+++ hw1/routes.py
@@ -72,19 +72,25 @@ def search():
 # Cria os gráficos
 @app.route("/scrape/graph")
 def create_graph():
-
+    # retira o número máximo de linhas para pot com Altair
+    alt.data_transformers.disable_max_rows()
+    
     # faz query no banco de dados
     autores = queryDB('author', ['ID_author','author'])
+    artigos = queryDB('paper', ['ID_paper','paper'])
     author_paper = queryDB('author_paper', ['ID_paper','ID_author'])
     
+    autores['ID_author'] = autores['ID_author'].astype(str)
+    artigos['ID_paper'] = artigos['ID_paper'].astype(str)
 
     ### renderiza os gráficos
     
-    ## Grafo 1
+    ## Grafo 1 - Autores (authors)
     graph = nx.Graph()
     
     # dataframe com colunas: paper e [lista_autores]
     group = pd.DataFrame(author_paper.groupby('ID_paper')['ID_author'].apply(list))
+    
     
     # Adicionando "edges"
     for j,row in group.iterrows():
@@ -93,33 +99,100 @@ def create_graph():
             for k in range(i,len(row['ID_author'])):
                 graph.add_edge(row['ID_author'][i], row['ID_author'][k])
                 
-    # coletando labels
-    autores['ID_author'] = autores['ID_author'].astype(str)
-    labels = autores.set_index('ID_author').to_dict()['author']
+    pos = nx.spring_layout(graph,k=0.2, iterations=50, weight=0.1, center=(0.5,0.5)) # forces graph layout
+    
+    # coletando nodes
+    nodes = to_pandas_nodes(graph,pos)
+    nodes.reset_index(inplace=True)
+    nodes.rename(columns={'index':'ID_author'}, inplace=True)
+    nodes = pd.merge(nodes,autores,on='ID_author')
+    
+    # coletando edges
+    edges = to_pandas_edges(graph,pos)
+    
+    
     
     # Gráfico 1
-    plt.figure(figsize=(8, 8))
+    
+    points = alt.Chart(nodes).mark_point(color='lightseagreen', fill='lightseagreen',size=50).encode(
+                alt.X('x', axis=alt.Axis(title='')),
+                alt.Y('y', axis=alt.Axis(title='')),
+                tooltip='author',
+                opacity=alt.value(0.6)
+            )
 
-    pos = nx.spring_layout(graph,k=0.2, iterations=50, weight=0.1, center=(0.5,0.5))
-    nx.draw_networkx(graph,pos= pos,  labels=labels, node_color='lightseagreen', edge_color='salmon',
-                    style="dotted", font_size=11)
+    lines = alt.Chart(edges).mark_line(color='salmon').encode(
+                alt.X('x', axis=alt.Axis(title='')),
+                alt.Y('y', axis=alt.Axis(title='')),
+                detail='edge',
+                opacity=alt.value(0.2)
+            )
 
-    #plt.title('Authors',{'fontsize':14})
-    plt.axis('off')
-    plt.savefig('static/img/authors.png', format='png')
+    chart = alt.LayerChart(layer=(lines,points)).properties(
+                height=300,
+                width=450
+                )
+    
+    
+    
+    
+    ## Grafo 2 - Artigos (papers)
+    graph1 = nx.Graph()
+    group1 = pd.DataFrame(author_paper.groupby('ID_author')['ID_paper'].apply(list))
+    
+    # Adicionando "edges"
+    for j,row in group1.iterrows():
+        i=len(row['ID_paper'])
+        for i in range(len(row['ID_paper'])):
+            for k in range(i,len(row['ID_paper'])):
+                graph1.add_edge(row['ID_paper'][i], row['ID_paper'][k])
+                
+    pos1 = nx.spring_layout(graph1,k=0.2, iterations=50, weight=0.1, center=(0.5,0.5))  # forces graph layout
+    
+    # coletando nodes
+    nodes1 = to_pandas_nodes(graph1, pos1)
+    nodes1.reset_index(inplace=True)
+    nodes1.rename(columns={'index':'ID_paper'}, inplace=True)
+    nodes1 = pd.merge(nodes1,artigos,on='ID_paper')
+    
+    # coletando edges
+    edges1 = to_pandas_edges(graph1,pos1)
+    
+    
+    
+    # Gráfico 2
+    
+    points1 = alt.Chart(nodes1).mark_point(color='coral', fill='coral',size=50).encode(
+                alt.X('x', axis=alt.Axis(title='')),
+                alt.Y('y', axis=alt.Axis(title='')),
+                tooltip='paper',
+                opacity=alt.value(0.6)
+            )
 
-    plot_url = "static/img/authors.png"
+    lines1 = alt.Chart(edges1).mark_line(color='lightblue', style='--').encode(
+                alt.X('x', axis=alt.Axis(title='')),
+                alt.Y('y', axis=alt.Axis(title='')),
+                detail='edge',
+                opacity=alt.value(0.2)
+            )
 
-    a = pd.DataFrame(pos.values(), columns=['edge_x','edge_y'])
-    a['label'] = list(labels.values())
 
-    chart = alt.Chart(a).mark_point(color='lightseagreen').encode(
-             x='edge_x',
-            y='edge_y',
-            tooltip='label'
-            ).interactive()
+    chart1 = alt.LayerChart(layer=(lines1,points1)).properties(
+                height=300,
+                width=450
+                )
+    
+    
+    ### Concatenando horizontamnete os gráficos 1 e 2
+    horiz_chart = alt.hconcat(chart, chart1 ).configure_axis( ticks=False,
+                grid=False,
+                domain=False,
+                labels=False).configure_view(
+                strokeWidth=0   
+            )
 
-    return chart.to_json()
+
+    return horiz_chart.to_json()
 
 
 
